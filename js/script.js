00f30bb8141565ad5914649b2d7a7c7f349929e2
Vương Đình Hiếu <113vuonghieu555@gmail.com>
2021-07-26 01:02:40 +0700

    Completed

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*----------- JS Blog Slider ------------*/
+$(document).ready(function(){
+    var data = {
+      dots: true,
+      arrows: false,
+      speed: 300,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      centerMode: true,
+      variableWidth: false,
+    };
+    $(".blog-slider").slick(data);
+
+  //   $('.menu-icon').click(function (e) { 
+  //     e.preventDefault();
+  //     $('.row').addClass('active');
+  //     $('.cover').addClass('active');
+  // });
+  });
+/*----------- End JS Blog Slider ------------*/
+
+/*----------- Header Scroll -------------*/
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+  } else {
+      document.getElementById("header").style.top = "-105px";
+  }
+  prevScrollpos = currentScrollPos;
+}     
+/*----------- End Header Scroll -------------*/
+
+/*------------- Menu ---------------*/
+
+/*------------- End Menu ---------------*/
